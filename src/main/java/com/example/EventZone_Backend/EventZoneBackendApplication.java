c12f3c9c17794e6ae316df51157bbe79c6e39c66
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-07-28 16:47:14 +0530

    Initial Commit

---
+++ src/main/java/com/example/EventZone_Backend/EventZoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventZone_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventZoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventZoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventZone_Backend/EventZoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventZone_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventZoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
