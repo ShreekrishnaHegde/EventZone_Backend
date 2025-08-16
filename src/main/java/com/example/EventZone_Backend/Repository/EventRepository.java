1a8c4d71002a455699a1cf0cfe4581a121fa4388
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-08-16 11:56:41 +0530

    Repo

--- src/main/java/com/example/EventZone_Backend/Repository/EventRepository.java
+++ src/main/java/com/example/EventZone_Backend/Repository/EventRepository.java
@@ -1,15 +1,14 @@
 package com.example.EventZone_Backend.Repository;
 
-import com.example.EventZone_Backend.Entity.Attendee;
 import com.example.EventZone_Backend.Entity.Event;
-import com.example.EventZone_Backend.Entity.Host;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface EventRepository extends MongoRepository<Event, ObjectId> {
-//    List<Event> findByHost(Host host);
+    Page<Event> findByHostId(ObjectId hostId, Pageable pageable);
 }
